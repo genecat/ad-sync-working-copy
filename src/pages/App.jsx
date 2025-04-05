e1100e0f0b0de6607e6d044337ef2debec4c6c73
genecatrambone <genecat@gmail.com>
2025-04-04 18:02:01 -1000

    Fix correct path to ModifyListing component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -7,7 +7,7 @@ import AuthForm from "./AuthForm";
 import SignUp from "./SignUp";
 import CreateListingFinal from "../components/CreateListingFinal";
 import NewPublisherDashboard from "../components/NewPublisherDashboard";
-import EditListing from "../ModifyListing"; // ✅ Corrected import path
+import EditListing from "./ModifyListing"; // ✅ Correct path from within src/pages
 import AdvertiserDashboard from "./AdvertiserDashboard";
 import CreateCampaign from "./CreateCampaign";
 import CampaignDashboard from "./CampaignDashboard";
